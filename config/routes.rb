89bf70a467da6eff791da44aaa5a238fc1ee25b7
Keegan Noble <keegannoble90@gmail.com>
2020-06-29 15:46:55 +0800

    initialized routing info

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :dogs, only: [:new, :create, :delete, :index, :edit, :update] do
+    resources :applications, only: [:new, :create]
+  end
+
+  resources :applications, only: [:edit, :update] do
+    resources :entries, except: [:show, :destroy]
+    resources :reviews, only: [:new, :create]
+  end
+
+  get '/dashboard', to: 'pages#dashboard'
 end
