e804b8db19bba15280c3e891267972c83bb6b814
Harold Seah <harryseahjy@gmail.com>
2020-06-29 18:28:16 +0800

    Coded the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+
+    user = User.create!(email: Faker::Internet.email, password: 'password')
+
+    dog = Dog.create!(name: Faker::Creature::Dog.name, breed: Faker::Creature::Dog.breed, age: rand(1..10), description: Faker::Lorem.sentence(word_count: 10), user: user)
+
+    request = Request.create!(status: 'pending', content: Faker::Lorem.sentence(word_count:10), start_date: (Time.at(rand * Time.now.to_i)), end_date: Time.now, user: user, dog: dog)
+
+end
+
+
+
+
+
+
