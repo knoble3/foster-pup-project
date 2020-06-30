41e932a5da21438114cdd81a90f10a5b847d3cc3
Keegan Noble <keegannoble90@gmail.com>
2020-06-30 12:37:10 +0800

    fixed Dog routes and Pundit authorizations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :dogs, only: [:new, :create, :delete, :index, :edit, :update, :show] do
+  resources :dogs do
     resources :requests, only: [:new, :create]
   end
 
