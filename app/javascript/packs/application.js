9d8142b72aee9ed086237b50799a395864af03bd
Alexander <alexander@Alex.local>
2020-07-02 13:40:22 +0700

    added popups to markers

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,6 +25,7 @@ require("channels")
 // External imports
 import "bootstrap";
 import {initMapbox} from '../plugins/init_mapbox'
+// import{initAutocomplete} from '..plugins/init_autocomplete'
 import { initUpdateNavbarOnScroll } from "../dom/dynamicNavbar"
 
 
@@ -35,5 +36,6 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox()
+  // initAutocomplete()
   initUpdateNavbarOnScroll()
 });
