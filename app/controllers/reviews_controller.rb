4a26861c20594fd9087e9d0ab79dc4147c64356f
Taylor Fong <taylor.fong.2012@gmail.com>
2020-07-02 15:02:51 +1000

    fixed bug in the dasg regarding photos

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,9 @@
 class ReviewsController < ApplicationController
   def new
+    @request = Request.find(params[:request_id])
     @review = Review.new
+    @review.request = @request
+    authorize @review
   end
 
   def create
@@ -9,11 +12,12 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.request = @request
     authorize @review
+    if @re
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
